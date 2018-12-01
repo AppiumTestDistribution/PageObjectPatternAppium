2b5f541ded8496cc34240224b456ebe41ecd2c68
saikrishna321 <saikrishna321@yahoo.com>
2018-12-01 14:47:40 +0000

    fetch ATD depdency from maven

--- src/test/java/com/appium/steps/RunnerCukes.java
+++ src/test/java/com/appium/steps/RunnerCukes.java
@@ -1,13 +1,15 @@
 package com.appium.steps;
 
 import com.appium.manager.ParallelThread;
-import org.junit.Test;
+import org.testng.annotations.Test;
+
 
 /**
  * Created by saikrisv on 22/04/16.
  */
 public class RunnerCukes {
-    @Test public void testCukesRunner() throws Exception {
+    @Test
+    public void testCukesRunner() throws Exception {
         ParallelThread parallelThread = new ParallelThread();
         parallelThread.runner("");
     }
